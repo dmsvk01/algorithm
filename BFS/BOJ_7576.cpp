3a823c4f157e9fc9b2e01075983c29e0f330b304
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-07-06 18:08:36 +0900

    Update BOJ_7576.cpp

--- BFS/BOJ_7576.cpp
+++ BFS/BOJ_7576.cpp
@@ -5,69 +5,105 @@
 
 using namespace std;
 
-queue<int> q[10];
-int input[1002][1002]={0};
-int v[1001][1001]={0};
+queue<pair<int, int>> q;
+int input[1000][1000]={0};
+int v[1000][1000]={0};
+vector<pair<int, int>> start;
+int m,n;
 
+int bfs(vector<pair<int, int>> start){
+    int k=0, count=0;
+    int kx, ky;
+    for(int i=0; i<start.size(); i++){
+        int x,y;
+        x = start[i].first;
+        y = start[i].second;
+        v[x][y]=1;
+        q.push(make_pair(x,y));
+    }
+    if(start.size()>=1){
+        kx = start[0].first; // Put key value 
+        ky = start[0].second;
+    }
+    q.push(make_pair(kx, ky));
+    //printf("kx : %d, ky : %d\n", kx, ky);
+    while(q.size() > 1){ // 1이면 종료
+        k++;
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        //printf("%d\n",k);
+        //printf("x : %d, y : %d\n",x,y);
+        if(k!=1 && x==kx && y==ky){
+            count++;
+            q.push(make_pair(kx,ky));
+        }else{
+            //토마토가 안익었고 즉 0 이고 방문 안했으면 넣는다. 방문체크까지
+            if(input[x-1][y]==0 && v[x-1][y]==0 && x!=0){ // 위쪽 
+                v[x-1][y]=1;
+                q.push(make_pair(x-1, y));
+                input[x-1][y]=1;
+            }
+            if(input[x+1][y]==0 && v[x+1][y]==0&&x!=n-1 ){ // 아래쪽 
+                v[x+1][y]=1;
+                q.push(make_pair(x+1, y));
+                input[x+1][y]=1;
+            }
+            if(input[x][y-1]==0 && v[x][y-1]==0 &&y!=0){ // 왼쪽
+                v[x][y-1]=1;
+                q.push(make_pair(x, y-1));
+                input[x][y-1]=1;
+            }
+            if(input[x][y+1]==0 && v[x][y+1]==0 && y!=m-1){ // 오른쪽 
+                v[x][y+1]=1;
+                q.push(make_pair(x, y+1));
+                input[x][y+1]=1;
+            }
+        }
+    }
+    return count;
+}
 int main(){
-  int m,n; cin >> m >> n;
+  cin >> m >> n;
 
-//input, v테두리 -1로 만들기
-  for(int i=0; i<=m+1; i++){
-    input[0][i]=-1;
-    input[n+1][i]=-1;
-    v[0][i]=1;
-    v[n+1][i]=1;
-  }
-  for(int i=0; i<=n+1; i++){
-    input[i][0]=-1;
-    input[i][m+1]=-1;
-    v[i][0]=1;
-    v[i][m+1]=1;
-  }
 //입력받기
-  for(int i=1; i<=n; i++){
-    for(int j=1; j<=m; j++){
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
       cin >> input[i][j];
     }
   }
-//test code
-  for(int i=0; i<=n+1; i++){
-    for(int j=0; j<=m+1; j++){
-      printf("%d ", input[i][j]);
+  
+  // 익은 토마토는 방문할 필요 없으므로 방문체크한다.
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      if(input[i][j]==1)
+        start.push_back(make_pair(i,j));
     }
-    printf("\n");
   }
-  //불가능 체크
-  for(int i=1; i<=n; i++){
-    for(int j=1; j<=m; j++){
-      if(input[i-1][j]==-1 && input[i+1][j]==-1 && input[i][j-1]==-1 && input[i][j+1]==-1){
-        cout << "-1" << "\n";
+  
+  int result = bfs(start);
+  
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      if(input[i][j]==0){
+        printf("-1\n");
         return 0;
       }
     }
   }
-  // 익은 토마토는 방문할 필요 없으므로 방문체크한다.
-  for(int i=1; i<=n; i++){
-    for(int j=1; j<=m; j++){
-      if(input[i][j]==1)
-        v[i][j]=1;
-    }
-  }
   
-  // bfs
-  // how can i do?
+  printf("%d\n", result);
+  
+  
+  return 0;
 
 }
 
 /*
-
 6 4
 1 -1 0 0 0 0
 0 -1 0 0 0 0
 0 0 0 0 -1 0
 0 0 0 0 -1 1
-
 6
-
 */
