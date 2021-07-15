e8156f727557a491b60a41e8a5ff8710ca0d98fd
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-07-15 12:41:41 +0900

    Create BOJ_11279.cpp

---
+++ priorityQueue/BOJ_11279.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <utility>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  priority_queue<int, vector<int>, less<int>> q;
+  int n; cin >> n;
+  for(int x,i=0; i<n; i++){
+    scanf("%d", &x);
+    if(x!=0){
+      q.push(x);
+    }else{
+      if(q.empty()==true){
+        printf("0\n");
+      }else{
+        printf("%d\n", q.top());
+        q.pop();
+      }
+    }
+  }
+
+}
