a695c7863d52ed49b065e66c8eaa599b0a031d0e
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-07-17 18:10:38 +0900

    Update BOJ_1753(fail).cpp

--- Dijkstra/BOJ_1753(fail).cpp
+++ Dijkstra/BOJ_1753(fail).cpp
@@ -1,50 +1,49 @@
 #include <iostream>
 #include <queue>
+#include <utility>
 #include <algorithm>
 #include <vector>
+#define NUM 2100000000
 
 using namespace std;
 
-vector<pair<int, int>> input[20001];
 queue<int> q;
-int visit[20001]={0};
+int v;
+vector<pair<int,int>> a[20001]; 
 int res[20001];
+int visit[20001]={0};
 
-int dijk(int start, int v){
-    for(int i=0; i<20001; i++) res[i]=987654321; // 초기화
-    
-    q.push(start);
-    visit[start]=1;
-    res[start]=0; // 이거 때문 고생함 ㅜㅜ
-    while(!q.empty()){
-        int x  = q.front();
-        q.pop();
-        for(int i=0; i<input[x].size(); i++){
-            int a = input[x][i].first; // 어디 연결 되어있는지
-            int b = input[x][i].second; // 가중치
-            if(visit[a]==0){
-                visit[a]=1;
-                if(res[a] > res[x] + b){ 
-                    res[a] = res[x] + b;
-                }
-                q.push(a);
-            }
-        }
+void bfs(int start){
+  for(int i=1; i<=v; i++) res[i]=NUM;
+  q.push(start);
+  res[start]=0;
+  visit[start]=1;
+  while(!q.empty()){
+    int x = q.front();
+    q.pop();
+    for(int i=0; i<a[x].size(); i++){
+      int y = a[x][i].first;
+      int w = a[x][i].second;
+      if(visit[y]==0){
+        q.push(y);
+        visit[y]=1;
+      }
+      res[y] = min(res[y], res[x]+w);
     }
-    //res가 다 채워짐 
-    for(int i=1; i<=v; i++){
-        if(res[i]==987654321) printf("INF ");
-        else printf("%d ", res[i]);
-    }
+  }
+  for(int i=1; i<=v; i++){
+    if(res[i]==NUM) printf("INF\n");
+    else printf("%d\n", res[i]);
+  }
 }
 
-
 int main(){
-    int v,e; cin >> v >> e;
-    int start; cin >> start;
-    for(int a,b,k,i=0; i<e; i++){
-        cin >> a >> b >> k;
-        input[a].push_back({b, k});
-    }
-    dijk(start, v);
+  int e; cin >> v >> e;
+  int start; cin >> start;
+  for(int x,y,w,i=0; i<e; i++){
+    scanf("%d %d %d",&x,&y,&w);
+    a[x].push_back(make_pair(y,w));
+  }
+  bfs(start);
+  return 0;
 }
