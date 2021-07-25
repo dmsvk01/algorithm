507f53288ff98eace6fdbb6344d846d9221c9651
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-07-25 14:48:34 +0900

    Update BOJ_11404(fail).cpp

--- floyd_warshall/BOJ_11404(fail).cpp
+++ floyd_warshall/BOJ_11404(fail).cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 #define VMAX 101
-#define INF 2100000000
+#define INF 987654321
 
 using namespace std;
 
@@ -12,27 +14,26 @@ void floyd_warshall(){
   int dist[VMAX][VMAX];
   for(int i=1; i<=n; i++)
     for(int j=1; j<=n; j++)
-      dist[i][j]=INF;
+      dist[i][j]= i==j? 0 : INF;
   
   for(int i=0; i<e; i++){
     int x = a[i].first.first;
     int y = a[i].first.second;
     int w = a[i].second;
     //printf("x:%d, y:%d w:%d\n",x,y,w);
-    dist[x][y] = w;
+    if(dist[x][y]!=INF) dist[x][y] = min(dist[x][y], w);
+    else dist[x][y] = w;
   }
-  for(int i=1; i<=n; i++){ // i 를 거쳐서 가는
-    for(int j=1; j<=n; j++){ // j 와
+  for(int i=1; i<=n; i++) // i 를 거쳐서 가는
+    for(int j=1; j<=n; j++) // j 와
       for(int k=1; k<=n; k++){ // k 사이의 dist
-          if(dist[j][k] > dist[j][i] + dist[i][k]){
-            dist[j][k] = dist[j][i] + dist[i][k];
-          }
+      if(i==j || i==k || j==k) continue;
+            dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
       }
-    }
-  }
+        
   for(int i=1; i<=n; i++){
     for(int j=1; j<=n; j++){
-      printf("%d ", dist[i][j]);
+      dist[i][j]==INF?printf("0 ") : printf("%d ",dist[i][j]);
     }
     printf("\n");
   }
@@ -48,22 +49,3 @@ int main(){
   }
   floyd_warshall();
 }
-
-/*
-5
-14
-1 2 2
-1 3 3
-1 4 1
-1 5 10
-2 4 2
-3 4 1
-3 5 1
-4 5 3
-3 5 10
-3 1 8
-1 4 2
-5 1 7
-3 4 2
-5 2 4
-*/
