3737a333105b5592e7b79a60056e1bd35175f7ff
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-07-30 20:10:12 +0900

    Update BOJ_9663(ing).cpp

--- Backtracking/BOJ_9663(ing).cpp
+++ Backtracking/BOJ_9663(ing).cpp
@@ -1,38 +1,38 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int count=0;
-int n;
+int n, count=0;
+int b[15]={0}; // max_N : 14
 
-void bt(int depth, int b[][16]){
-  if(depth==n+1) count++;
-  for(int i=1; i<=n; i++){
-    bool pass = true;
-    int x=i, y=depth;
+bool check(int pos){
+    for (int i = 1; i < pos; i++) {
+		if (b[pos] == b[i] || pos - i == abs(b[pos] - b[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+void bt(int pos){
+    if(pos==n+1){
+        count++;
+        return ;
+    }
+    
     for(int i=1; i<=n; i++){
-      if(b[x][i]==1 || b[i][y]==1) pass = false;
-      if(x+i<=n && y+i<=n && b[x+i][y+i]==1) pass = false;
-      if(x+i<=n && y-i>=1 && b[x+i][y-i]==1) pass = false;
-      if(x-i>=1 && y+i<=n && b[x-i][y+i]==1) pass = false;
-      if(x-i>=1 && y-i>=1 && b[x-i][y-i]==1) pass = false;
+        b[pos]=i;
+        if(check(pos)==true){
+            bt(pos+1);
+        }
     }
-    if(pass){
-      b[i][depth]=1;
-      bt(depth+1, b);
-      b[i][depth]=0;
-    }
-  }
-  return ;
+    
+    return ;
 }
 
 int main(){
-  int b[16][16]={0};
-  cin >> n;
-  bt(1, b);
-  printf("%d", count);
-  return 0;
+    cin >> n;
+    bt(1);
+    printf("%d", count);
+    return 0;
 }
-
-//(1 ≤ N < 15)
