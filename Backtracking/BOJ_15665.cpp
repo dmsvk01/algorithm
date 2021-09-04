e88e39822f1ff660ee0fca018fd4d42e5413d3fb
dmsvk01 <62129820+dmsvk01@users.noreply.github.com>
2021-09-04 15:09:09 +0900

    Create BOJ_15665.cpp

---
+++ Backtracking/BOJ_15665.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n,m;
+vector<int> input;
+vector<int> result;
+
+void bt(int depth){
+  if(depth==m){
+    for(int i=0; i<result.size(); i++){
+      printf("%d ", result[i]);
+    }
+    printf("\n");
+    return ;
+  }
+  int mul = -1;
+  for(int i=0; i<n; i++){
+    if(mul == input[i]) continue;
+    mul = input[i]; 
+    result.push_back(input[i]);
+    bt(depth+1);
+    result.pop_back();
+  }
+  return ;
+}
+
+int main(){
+  cin >> n >> m;
+  for(int x,i=0; i<n; i++){
+    scanf("%d", &x);
+    input.push_back(x);
+  }
+  sort(input.begin(), input.end());
+  bt(0);
+  return 0;
+}
